ab916e5a2a0d9950fcf0f255def78d0d74a74d78
Dimmi <dima.baturo1@gmail.com>
2020-10-05 00:21:04 +0300

    feat: almost finished vigenere ciphere

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -2,22 +2,50 @@ const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
   encrypt(string1,string2) {
+    let index = 0;
     let arr = []; 
-    string1 = string1.replace(/\s/gi,"");
     for(let i = 0;i<string1.length;i++){
-      let letter = string1.toUpperCase().codePointAt(i) + string2.toUpperCase().codePointAt(i) - "A".codePointAt(0);
-      if(letter < 90){
-        arr.push(String.fromCharCode(letter))
+      if(string1.toUpperCase()[i].codePointAt() < 91 && string1.toUpperCase()[i].codePointAt()>64){
+        let letter = string1.toUpperCase().codePointAt(i) + string2.toUpperCase().codePointAt(index) - "A".codePointAt(0);
+        index = index + 1;
+        if(index > string2.length-1){
+          index = 0;
+        }
+        if(letter < 91){
+          arr.push(String.fromCharCode(letter))
+        }
+        else{
+          arr.push(String.fromCharCode(letter-26))
+        }
       }
       else{
-        arr.push(String.fromCharCode(letter-26))
+        arr.push(string1[i])
       }
     }
 	  return arr.join("");
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(string1,string2) {
+    let index = 0;
+    let arr = []; 
+    for(let i = 0;i<string1.length;i++){
+      if(string1.toUpperCase()[i].codePointAt() < 91 && string1.toUpperCase()[i].codePointAt()>64){
+        let letter = string1.toUpperCase().codePointAt(i) - (string2.toUpperCase().codePointAt(index) - "A".codePointAt(0));
+        index = index + 1;
+        if(index > string2.length-1){
+          index = 0;
+        }
+        if(letter > 64){
+          arr.push(String.fromCharCode(letter))
+        }
+        else{
+          arr.push(String.fromCharCode(letter+26))
+        }
+      }
+      else{
+        arr.push(string1[i])
+      }
+    }
+	  return arr.join("");
   }
 }
 
